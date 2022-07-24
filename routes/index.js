a8223411a85049aed44771c103c0723a39fafa7b
warriorjacq9 <jackflusche@gmail.com>
2022-07-24 12:56:31 -0700

    Initial commit.

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'WJ9 Studios' });
+});
+
+module.exports = router;
